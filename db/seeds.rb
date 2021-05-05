b2ed065b9a957a26ae3e351a71c8eb2b7dfdfe2f
rin1782 <colaerin@yahoo.com>
2021-05-04 22:56:36 -0400

    seed data entered

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Kid.create(name: "Aspen")
+Kid.create(name: "Darringer")
+Kid.create(name: "Finnley")
+
+Chore.create(name: "Sweep Deck", kid_id: 1)
+Chore.create(name: "Empty Dishwasher", kid_id: 1)
+Chore.create(name: "Fold Laundry", kid_id: 2)
+Chore.create(name: "Water Flowers", kid_id: 2)
+Chore.create(name: "Feed Dog", kid_id: 3)
+Chore.create(name: "Wipe Windows", kid_id: 3)
