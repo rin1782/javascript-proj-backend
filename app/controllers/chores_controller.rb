ad8edd57fe81d7c8ed625e784ad1908a5a44d0a3
rin1782 <colaerin@yahoo.com>
2021-05-05 00:19:10 -0400

    backend API created

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -3,4 +3,38 @@ class ChoresController < ApplicationController
     def index
         render(json: Chore.all)
     end
+
+    def create
+        @chore = Chore.new(chore_params)
+    
+        if @chore.save
+          render json: @chore, status: :created, location: @chore
+        else
+          render json: @chore.errors, status: :unprocessable_entity
+        end
+    end
+    
+      
+    def update
+        if @chore.update(chore_params)
+          render json: @chore
+        else
+          render json: @chore.errors, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @chore.destroy
+    end
+    
+    private
+        # Use callbacks to share common setup or constraints between actions.
+    def set_chore
+        @chore = Chore.find(params[:id])
+    end
+    
+        # Only allow a list of trusted parameters through.
+    def chore_params
+        params.require(:chore).permit(:id, :name, :kid_id)
+    end
 end
--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -8,4 +8,33 @@ class KidsController < ApplicationController
         kid = Kid.find_by(id: params[:id])
         render json: kid
     end
+
+    def create
+        @kid = Kid.create(kid_params)
+        if @kid.save
+          render json: @kid, status: :created, location: @kid
+        else
+          render json: @kid.errors, status: :unprocessable_entity
+        end
+    end
+    
+     
+    def update
+        if @kid.update(kid_params)
+          render json: @kid
+        else
+          render json: @kid.errors, status: :unprocessable_entity
+        end
+    end
+    
+     
+    def destroy
+        @kid.destroy
+    end
+    
+    private
+    
+    def kid_params
+        params.require(:kid).permit(:name, :id)
+    end
 end
